ebe331eee2b1647822dd8ce64b25f622f5d4766e
Luis Hernandez <43943030+LuisHernandez20@users.noreply.github.com>
2025-12-26 09:32:48 -0600

    Update scrip optimizacion.bat
    Implementacion para deshabilitacion de funciones windows search

--- scrip optimizacion.bat
+++ scrip optimizacion.bat
@@ -261,6 +261,16 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "VoiceShortcu
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "CortanaConsent" /t "REG_DWORD" /d "0" /f
 echo.
 echo ============================================================
+echo Deshabilitando windows search telemetria     
+echo ============================================================
+echo.
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableWebSearch /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v ConnectedSearchUseWeb /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /t REG_DWORD /d 0 /f
+echo.
+echo ============================================================
 echo Deshabilitando Telemetria de Office
 echo ============================================================
 echo.
